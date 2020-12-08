38cf6741fe013209d6d3a9bd54fa5e0e183e60ff
Madhav Bansal <madhav@madhav.successivetech.com>
2020-12-08 21:37:14 +0530

    updating iphone view

--- src/customHooks/useDevice.js
+++ src/customHooks/useDevice.js
@@ -2,23 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 
 const useDevice = () => {
-  const [deviceType, setDeviceType] = useState('desktop');
-  // Note: This is for getting  device type 
-  useEffect(() => {
-      const getDeviceType = () => {
-          let device = 'desktop';
-          const { innerWidth } = window || {}
-          if (innerWidth && innerWidth <= 990) device = 'mobile';
-          else if (innerWidth && innerWidth <= 1035) device = 'tab';
-          setDeviceType(device)
+    const [deviceType, setDeviceType] = useState('desktop');
+    // Note: This is for getting  device type 
+    useEffect(() => {
+        const getDeviceType = () => {
+            let device = 'desktop';
+            const { screen: { availWidth } = {} } = window || {}
+            if (availWidth < 760) device = 'mobile';
+            if (availWidth > 760 && availWidth < 1400) device = 'tab';
 
-      }
-      window.addEventListener("resize", getDeviceType);
-      return () => {
-          window.removeEventListener("resize", getDeviceType);
-      }
-  }, [deviceType]);
-    
+            setDeviceType(device)
+
+        }
+        window.addEventListener("resize", getDeviceType);
+        return () => {
+            window.removeEventListener("resize", getDeviceType);
+        }
+    }, [deviceType]);
+
     return deviceType;
 }
 
